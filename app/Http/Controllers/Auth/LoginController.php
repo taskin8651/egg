5fbd814b9bb849e8aa83ca3de53cd754c22efe02
taskin8651 <mdsayebalam10@gmail.com>
2026-04-18 20:36:51 +0530

    sdsd

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,26 +3,38 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
     use AuthenticatesUsers;
 
-    protected $redirectTo = '/'; // ye ignore ho jayega authenticated() ke baad
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
 
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    // 🔥 ROLE BASED REDIRECT
-    protected function authenticated($request, $user)
-    {
-        if ($user->role_id == 1) {
-            return redirect('/admin/dashboard');
-        }
-
-        return redirect('/user/dashboard');
     }
 }
